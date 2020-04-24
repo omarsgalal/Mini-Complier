f3805186c62036cf256b7aeafb0ab1baeb8ad210
abdokaseb <abdokaseb@gmail.com>
2020-04-25 00:00:10 +0200

    add print quadruples in the ex function

--- parser.c
+++ parser.c
@@ -2,45 +2,114 @@
 #include "includes.h"
 #include "y.tab.h"
 
+static int lbl;
+
 int ex(nodeType *p) {
+    int lbl1, lbl2;
+
     if (!p) return 0;
     switch(p->type) {
     case typeCon:  
         switch(p->con.type) {
-            case INTEGER:       return p->con.value;
-            case FLOAT:         return p->con.valueF;
-            case CHARACTER:     return p->con.valueC;
+            case INTEGER:       printf("\tpush\t%d\n", p->con.value); break;
+            case FLOAT:         printf("\tpush\t%f\n", p->con.valueF); break;
+            case CHARACTER:     printf("\tpush\t%c\n", p->con.valueC); break;
         }     
     
-    case typeId:        return sym[p->id.i]->value;
+    case typeId:        printf("\tpush\t%c\n", p->id.i + 'a');  break;
     case typeOpr:
         switch(p->opr.oper) {
-        case WHILE:     while(ex(p->opr.op[0])) ex(p->opr.op[1]); return 0;
-        case DOWHILE:   do {ex(p->opr.op[1]);} while(ex(p->opr.op[0])); return 0;
-        case FOR:       for(ex(p->opr.op[0]); ex(p->opr.op[1]); ex(p->opr.op[2])) ex(p->opr.op[3]); return 0;
-        case IF:        if (ex(p->opr.op[0]))
-                            ex(p->opr.op[1]);
-                        else if (p->opr.nops > 2)
-                            ex(p->opr.op[2]);
-                        return 0;
-        case PRINT:     printf("%d\n", ex(p->opr.op[0])); return 0;
-        case ';':       ex(p->opr.op[0]); return ex(p->opr.op[1]);
-        case '=':       return sym[p->opr.op[0]->id.i]->value = ex(p->opr.op[1]);
-        case UMINUS:    return -ex(p->opr.op[0]);
-        case '+':       return ex(p->opr.op[0]) + ex(p->opr.op[1]);
-        case '-':       return ex(p->opr.op[0]) - ex(p->opr.op[1]);
-        case '*':       return ex(p->opr.op[0]) * ex(p->opr.op[1]);
-        case '/':       return ex(p->opr.op[0]) / ex(p->opr.op[1]);
-        case '%':       return ex(p->opr.op[0]) % ex(p->opr.op[1]);
-        case '<':       return ex(p->opr.op[0]) < ex(p->opr.op[1]);
-        case '>':       return ex(p->opr.op[0]) > ex(p->opr.op[1]);
-        case AND:       return ex(p->opr.op[0]) && ex(p->opr.op[1]);
-        case OR:        return ex(p->opr.op[0]) || ex(p->opr.op[1]);
-        case '!':       return !ex(p->opr.op[0]);
-        case GE:        return ex(p->opr.op[0]) >= ex(p->opr.op[1]);
-        case LE:        return ex(p->opr.op[0]) <= ex(p->opr.op[1]);
-        case NE:        return ex(p->opr.op[0]) != ex(p->opr.op[1]);
-        case EQ:        return ex(p->opr.op[0]) == ex(p->opr.op[1]);
+        case WHILE:     
+            printf("L%03d:\n", lbl1 = lbl++);
+            ex(p->opr.op[0]);
+            printf("\tjz\tL%03d\n", lbl2 = lbl++);
+            ex(p->opr.op[1]);
+            printf("\tjmp\tL%03d\n", lbl1);
+            printf("L%03d:\n", lbl2);
+            break;
+        case DOWHILE:   
+            printf("L%03d:\n", lbl1 = lbl++);
+            ex(p->opr.op[1]);
+            ex(p->opr.op[0]);
+            printf("\tjz\tL%03d\n", lbl2 = lbl++);
+            printf("\tjmp\tL%03d\n", lbl1);
+            printf("L%03d:\n", lbl2);
+            break;
+        case FOR:       
+            // for(ex(p->opr.op[0]); ex(p->opr.op[1]); ex(p->opr.op[2])) ex(p->opr.op[3]); return 0;
+            ex(p->opr.op[0]);
+            printf("L%03d:\n", lbl1 = lbl++);
+            ex(p->opr.op[2]);
+            printf("\tjz\tL%03d\n", lbl2 = lbl++);
+            ex(p->opr.op[1]);
+            printf("\tjmp\tL%03d\n", lbl1);
+            printf("L%03d:\n", lbl2);
+            break;
+
+        case IF:        
+            // if (ex(p->opr.op[0]))
+            //     ex(p->opr.op[1]);
+            // else if (p->opr.nops > 2)
+            //     ex(p->opr.op[2]);
+            // return 0;
+            if (p->opr.nops > 2)
+            {
+                ex(p->opr.op[0]);
+                printf("\tjz\tL%03d\n", lbl2 = lbl++);
+                ex(p->opr.op[1]);
+                printf("\tjmp\tL%03d\n", lbl1 = lbl++);
+                printf("L%03d:\n", lbl2);
+                ex(p->opr.op[2]);
+                printf("L%03d:\n", lbl1);
+            } else {
+                ex(p->opr.op[0]);
+                printf("\tjz\tL%03d\n", lbl2 = lbl++);
+                ex(p->opr.op[1]);
+                printf("L%03d:\n", lbl2);
+            }
+            break;
+
+        case PRINT:     
+            // printf("%d\n", ex(p->opr.op[0])); return 0;
+            ex(p->opr.op[0]);
+            printf("\tprint\n");
+            break;
+        case ';':       
+            ex(p->opr.op[0]); return ex(p->opr.op[1]);
+        case '=':       
+            // return sym[p->opr.op[0]->id.i]->value = ex(p->opr.op[1]);
+            ex(p->opr.op[1]);
+            printf("\tpop\t%c\n", p->opr.op[0]->id.i + 'a');
+            break;
+
+        case UMINUS:    
+            // return -ex(p->opr.op[0]);
+            ex(p->opr.op[0]);
+            printf("\tneg\n");
+            break;
+        case '!':       
+            // return !ex(p->opr.op[0]);
+            ex(p->opr.op[0]);
+            printf("\tnot\n");
+            break;
+        default:
+            ex(p->opr.op[0]);
+            ex(p->opr.op[1]);
+            switch(p->opr.oper) {
+            case '+':    printf("\tadd\n"); break;
+            case '-':    printf("\tsub\n"); break;
+            case '*':    printf("\tmul\n"); break;
+            case '/':    printf("\tdiv\n"); break;
+            case '%':    printf("\tmod\n"); break;
+            case '<':    printf("\tcompLT\n"); break;
+            case '>':    printf("\tcompGT\n"); break;
+            case AND:    printf("\tcompAND\n"); break;
+            case OR:     printf("\tcompOR\n"); break;
+            case GE:     printf("\tcompGE\n"); break;
+            case LE:     printf("\tcompLE\n"); break;
+            case NE:     printf("\tcompNE\n"); break;
+            case EQ:     printf("\tcompEQ\n"); break;
+            }
         }
     }
     return 0;
